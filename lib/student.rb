5bb9adbc2fb1f32598d9a57ddded18cea94bf435
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-06 05:08:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,11 +67,12 @@ class Student
 
   def self.first_student_in_grade_10
     sql = <<-SQL
-      SELECT id FROM students
+      SELECT * FROM students
       WHERE grade = 10
+      LIMIT 1
     SQL
 
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql).each do |row|
       self.new_from_db(row)
     end
   end
